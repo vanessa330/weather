ce8166877756c5102770ec4a97c7cdba8a0bad4a
vanessa330 <vanessa.33010@gmail.com>
2023-02-08 16:18:02 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Getting Started with Create React App
+
+> https://tailwindcss.com/docs/guides/create-react-app
+
+> https://github.com/IconScout/react-unicons
+
+> https://openweathermap.org/
---
+++ src/services/weatherService.js
@@ -0,0 +1,68 @@
+import { DateTime } from "luxon";
+
+const API_KEY = "42aa667528acd991472cac53d86e5609";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// https://api.openweathermap.org/data/2.5/weather?q=london&appid=42aa667528acd991472cac53d86e5609
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url).then((resp) => resp.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  const { lat, lon } = formattedCurrentWeather;
+
+  return { ...formattedCurrentWeather};
+};
+
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconUrlFromCode = (code) =>
+  `https://openweathermap.org/img/wn/${code}@2x.png`;
+  
+export default getFormattedWeatherData;
+export { formatToLocalTime, iconUrlFromCode };
